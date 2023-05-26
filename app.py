093da585db1fc397270a041fb26d29ed92692f15
Evergyu <iingy123@naver.com>
2023-05-26 15:10:45 +0900

    page moving

--- app.py
+++ app.py
@@ -25,11 +25,20 @@ query1= {"title" : "안녕하세요 보겸입니다"}
     
 @app.route('/') # 접속하는 url
 def index():
-    client = pymongo.MongoClient(f'mongodb://{user}:{urllib.parse.quote_plus(pwd)}@{host}:{port}/{db}')
-    db_conn = client.get_database(db)
-    collection = db_conn.get_collection("hi")
-    result = collection.find()
-    return render_template('hi.html')
+
+    name=request.args.get('radio')
+    order=request.args.get('radio2')
+    date=request.args.get('date')
+
+    print("-------------------------------")
+    print()
+    print(date,name,order)
+    print()
+    print("-------------------------------")
+    if order=="video":
+        return render_template('hi.html')
+    if order=="channel":
+        return render_template('hi2.html')
 
 @app.route('/bokeyem', methods=['POST']) # 접속하는 url
 def bokeyem():
