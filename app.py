fa5d6eda6cd7408bed073210f391f4b30c68587e
Evergyu <iingy123@naver.com>
2023-05-29 17:54:59 +0900

    layout changed

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import timedelta
 import urllib.parse
 from pymongo import ASCENDING
 
-#Flask 객체 인스턴스 생성!!
+# Flask 객체 인스턴스 생성!!
 app = Flask(__name__)
 
 host = "localhost"
@@ -63,56 +63,58 @@ hot = [
         '$limit': 1000
     }
 ]
-    
-@app.route('/') # 접속하는 url
+
+
+@app.route('/')  # 접속하는 url
 def index():
     return render_template('index.html')
 
 
-@app.route('/bokeyem', methods=['POST']) # 접속하는 url
+@app.route('/bokeyem', methods=['POST'])  # 접속하는 url
 def bokeyem():
     result = list(collection.aggregate(hot))
-    print(result)
-    return render_template('index.html', data=result)
+    condition = "2"
+    return render_template('index.html', data=result, condition=condition)
+
 
 @app.route('/search')
 def genre():
-    name=request.args.get('radio')
-    order=request.args.get('radio2')
-    startDate=request.args.get('startDate')
-    endDate=request.args.get('endDate')
-    genre=request.args.get('genre')
-    query2={"categoryId": int(genre)}
-    print(type(order))
+    name = request.args.get('radio')
+    order = request.args.get('radio2')
+    startDate = request.args.get('startDate')
+    endDate = request.args.get('endDate')
+    genre = request.args.get('genre')
+    query2 = {"categoryId": int(genre)}
+    condition = "1"
 
-    if startDate =='' and endDate == '' : #날짜 선택이 안됐을때
-        query2={"categoryId": int(genre)}
-        result=collection.find(query2)
+    if startDate == '' and endDate == '':  # 날짜 선택이 안됐을때
+        query2 = {"categoryId": int(genre)}
+        result = collection.find(query2)
         if order is None:
-            result.sort("view_count",-1)
+            result.sort("view_count", -1)
         else:
             result.sort(order, -1)
 
-    else:           #날짜 선택이 됐을때
-        if endDate=='': #시작날짜만 선택됐을때
-            startDate=datetime.strptime(startDate, '%Y-%m-%d')
-            query2={"publishedAt": {'$gte' : startDate}}
+    else:  # 날짜 선택이 됐을때
+        if endDate == '':  # 시작날짜만 선택됐을때
+            startDate = datetime.strptime(startDate, '%Y-%m-%d')
+            query2 = {"publishedAt": {'$gte': startDate}}
             result = collection.find(query2)
             if order is None:
                 result.sort("publishedAt", 1)
             else:
                 result.sort(order, -1)
-        elif startDate=='': #종료날짜만 선택됐을때
+        elif startDate == '':  # 종료날짜만 선택됐을때
             print("no")
-        #둘다 선택됐을때
+        # 둘다 선택됐을때
         else:
-            startDate=datetime.strptime(startDate, '%Y-%m-%d')
-            endDate=datetime.strptime(endDate, '%Y-%m-%d')
-            endDate=endDate.replace(hour=23, minute=59, second=59)
+            startDate = datetime.strptime(startDate, '%Y-%m-%d')
+            endDate = datetime.strptime(endDate, '%Y-%m-%d')
+            endDate = endDate.replace(hour=23, minute=59, second=59)
             print(endDate)
-            #endDate=startDate+timedelta(days=1)
-            query2={"publishedAt": {'$gte' : startDate, '$lte':endDate}}
-            result=collection.find(query2)
+            # endDate=startDate+timedelta(days=1)
+            query2 = {"publishedAt": {'$gte': startDate, '$lte': endDate}}
+            result = collection.find(query2)
             if order is None:
                 result.sort("publishedAt", 1)
             else:
@@ -126,13 +128,10 @@ def genre():
     print("-------------------------------")
     print(result)
 
-    return render_template('index.html', data=result)
-    
+    return render_template('index.html', data=result, condition=condition)
 
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
     # host 등을 직접 지정하고 싶다면
     # app.run(host="127.0.0.1", port="5000", debug=True)
